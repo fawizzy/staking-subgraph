2c1efb24f7e834e401ccc907b308ce3971295ee1
Fawaz Oduola <oduolafawaz@gmail.com>
2025-09-13 16:11:51 +0100

    fixed and made some changes to the schema

--- src/staking-contract.ts
+++ src/staking-contract.ts
@@ -1,4 +1,4 @@
-import { BigInt, Bytes } from "@graphprotocol/graph-ts";
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
   EmergencyWithdrawn as EmergencyWithdrawnEvent,
   RewardsClaimed as RewardsClaimedEvent,
@@ -6,55 +6,50 @@ import {
   StakingInitialized as StakingInitializedEvent,
   Withdrawn as WithdrawnEvent,
 } from "../generated/StakingContract/StakingContract";
-import {
-  EmergencyWithdrawn,
-  RewardsClaimed,
-  Staked,
-  Withdrawn,
-  StakingInitialized,
-  ProtocolStatitics,
-  UserInfo,
-} from "../generated/schema";
+import { ProtocolStatistics } from "../generated/schema";
 import { loadOrCreateUserInfo } from "./utils";
 
 export function handleEmergencyWithdrawn(event: EmergencyWithdrawnEvent): void {
-
   let userInfo = loadOrCreateUserInfo(event.params.user);
   userInfo.stakedAmount = BigInt.zero();
+  userInfo.pendingRewards = BigInt.zero();
+  userInfo.totalWithdrawn = userInfo.totalWithdrawn.plus(event.params.amount);
+  userInfo.updatedAt = event.block.timestamp;
 
-  let protocolStatistics = ProtocolStatitics.load("1");
+  let protocolStatistics = ProtocolStatistics.load("1");
   if (protocolStatistics) {
     protocolStatistics.totalStaked = event.params.newTotalStaked;
     protocolStatistics.save();
   }
 
-  userInfo.save()
-
+  userInfo.save();
 }
 
 export function handleRewardsClaimed(event: RewardsClaimedEvent): void {
- 
   let userInfo = loadOrCreateUserInfo(event.params.user);
   userInfo.pendingRewards = event.params.newPendingRewards;
+  userInfo.totalClaimed = userInfo.totalClaimed.plus(event.params.amount);
+  userInfo.updatedAt = event.block.timestamp;
 
-  let protocolStatistics = ProtocolStatitics.load("1");
+  let protocolStatistics = ProtocolStatistics.load("1");
   if (protocolStatistics) {
     protocolStatistics.totalStaked = event.params.totalStaked;
     protocolStatistics.save();
   }
 
   userInfo.save();
-  
 }
 
 export function handleStaked(event: StakedEvent): void {
-
   let userInfo = loadOrCreateUserInfo(event.params.user);
-  userInfo.stakedAmount.plus(event.params.amount);
-  userInfo.lastStake = event.params.timestamp;
- 
+  userInfo.stakedAmount = userInfo.stakedAmount.plus(event.params.amount);
+  userInfo.lastStakeTimestamp = event.params.timestamp;
+  if (userInfo.createdAt.equals(BigInt.zero())) {
+    userInfo.createdAt = event.block.timestamp;
+  }
+  userInfo.updatedAt = event.block.timestamp;
 
-  let protocolStatistics = ProtocolStatitics.load("1");
+  let protocolStatistics = ProtocolStatistics.load("1");
   if (protocolStatistics) {
     protocolStatistics.totalStaked = event.params.newTotalStaked;
     protocolStatistics.currentRewardRate = event.params.currentRewardRate;
@@ -65,13 +60,13 @@ export function handleStaked(event: StakedEvent): void {
 }
 
 export function handleWithdrawn(event: WithdrawnEvent): void {
- 
   let userInfo = loadOrCreateUserInfo(event.params.user);
-
-  userInfo.stakedAmount.minus(event.params.amount);
+  userInfo.stakedAmount = userInfo.stakedAmount.minus(event.params.amount);
   userInfo.pendingRewards = event.params.rewardsAccrued;
+  userInfo.totalWithdrawn = userInfo.totalWithdrawn.plus(event.params.amount);
+  userInfo.updatedAt = event.block.timestamp;
 
-  let protocolStatistics = ProtocolStatitics.load("1");
+  let protocolStatistics = ProtocolStatistics.load("1");
   if (protocolStatistics) {
     protocolStatistics.totalStaked = event.params.newTotalStaked;
     protocolStatistics.currentRewardRate = event.params.currentRewardRate;
@@ -82,13 +77,12 @@ export function handleWithdrawn(event: WithdrawnEvent): void {
 }
 
 export function handleStakingInitialized(event: StakingInitializedEvent): void {
-  let protocolStatistics = new ProtocolStatitics("1");
+  let protocolStatistics = new ProtocolStatistics("1");
   protocolStatistics.initialApr = event.params.initialRewardRate;
   protocolStatistics.currentRewardRate = event.params.initialRewardRate;
 
-  protocolStatistics.aprReductionPerThousand = BigInt.zero();
   protocolStatistics.totalStaked = BigInt.zero();
-  protocolStatistics.minLockDuration = BigInt.zero();
+  protocolStatistics.totalUsers = BigInt.zero();
 
   protocolStatistics.save();
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,24 @@
 import { BigInt, Bytes } from "@graphprotocol/graph-ts";
-import { UserInfo } from "../generated/schema";
+import { UserInfo, ProtocolStatistics } from "../generated/schema";
 
-export function loadOrCreateUserInfo(address: Bytes ): UserInfo{
-    let userInfo = UserInfo.load(address);
-    if (!userInfo){
-        userInfo = new UserInfo(address);
-        userInfo.lastStake = BigInt.fromI64(0);
-        userInfo.pendingRewards = BigInt.fromI64(0);
-        userInfo.stakedAmount = BigInt.fromI64(0);
-        userInfo.save();      
-    }
+export function loadOrCreateUserInfo(address: Bytes): UserInfo {
+  let userInfo = UserInfo.load(address);
 
-    return userInfo;
+  if (!userInfo) {
+    userInfo = new UserInfo(address);
+    userInfo.lastStakeTimestamp = BigInt.zero();
+    userInfo.pendingRewards = BigInt.zero();
+    userInfo.stakedAmount = BigInt.zero();
+    userInfo.save();
+
+    let protocolStats = ProtocolStatistics.load("1");
+   if (protocolStats){
+    protocolStats.totalUsers = protocolStats.totalUsers.plus(BigInt.fromI32(1));
+    protocolStats.save();
+
+   } 
+    
+  }
+
+  return userInfo;
 }
-
